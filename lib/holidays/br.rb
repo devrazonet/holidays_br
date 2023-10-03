5371d2dbf3759f22c30c5168969e765b584b1e60
IgorFZ <igorf.zamp@gmail.com>
2023-10-03 14:26:55 -0300

    feat: first commit

---
+++ lib/holidays/br.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "br/version"
+
+module Holidays
+  module Br
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/holidays/br/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Holidays
+  module Br
+    VERSION = "0.1.0"
+  end
+end
